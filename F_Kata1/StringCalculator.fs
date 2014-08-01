2cb3e1a21c59f4677c9d0314cc01d3a68c2f5b99
David Winslow <david.winslow@episerver.com>
2014-08-01 15:57:30 +0200

    Added initial solution

---
+++ F_Kata1/StringCalculator.fs
@@ -0,0 +1,8 @@
+﻿module StringCalculator
+
+open System
+let add numbers =
+    if (String.IsNullOrEmpty(numbers)) 
+        then 0
+    else
+        1
---
+++ F_Kata1/tests.fs
@@ -0,0 +1,16 @@
+﻿module tests
+
+open NUnit.Framework
+open StringCalculator
+
+[<Test>]
+let Should_return_0_for_empty_string() =
+    Assert.AreEqual(0, add "")
+[<Test>]
+let Should_return_value_of_first_parameter() =
+    Assert.AreEqual(1,add "1")
+
+[<Test>]
+let Should_return_sum_of_commaseparated_value() =
+    Assert.AreEqual( 3, add "1,2")
+
