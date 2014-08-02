18038697e64f24bebfba301d7a33ca2eb153df41
David Winslow <david.winslow@episerver.com>
2014-08-02 07:59:35 +0200

    Added logic for handling zero, one and many parameters. Handling for multiple separators

--- F_Kata1/StringCalculator.fs
+++ F_Kata1/StringCalculator.fs
@@ -1,8 +1,11 @@
 ﻿module StringCalculator
 
 open System
+open System.Linq
 let add numbers =
     if (String.IsNullOrEmpty(numbers)) 
         then 0
     else
-        1
+        let splitters = [|",";"\n"|]
+        numbers.Split(splitters,StringSplitOptions.RemoveEmptyEntries).Select(fun x -> Int32.Parse x ).Sum()
+        
--- F_Kata1/tests.fs
+++ F_Kata1/tests.fs
@@ -10,7 +10,16 @@ let Should_return_0_for_empty_string() =
 let Should_return_value_of_first_parameter() =
     Assert.AreEqual(1,add "1")
 
+[<TestCase("1,2",3)>]
+[<TestCase("2,4",6)>]
+let Should_return_sum_of_commaseparated_value(values,expected) =
+    Assert.AreEqual( expected, add values)
+
 [<Test>]
-let Should_return_sum_of_commaseparated_value() =
-    Assert.AreEqual( 3, add "1,2")
+let Should_split_on_line_breaks_and_Sum_it() =
+ Assert.AreEqual( 5, add "1\n2\n\n2")
+
+
+
+
 
